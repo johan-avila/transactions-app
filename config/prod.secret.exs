786d8ce636f4eda7660fa5c1af9fae56b7d7f06a
Johan <johan-avila@outlook.com>
2020-12-29 16:41:32 -0600

    ready for deployment

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -1,3 +1,4 @@
+
 # In this file, we load production configuration and secrets
 # from environment variables. You can also hardcode secrets,
 # although such is generally not recommended and you have to
@@ -14,7 +15,7 @@ database_url =
 config :transactions, Transactions.Repo,
   # ssl: true,
   url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "1")
 
 secret_key_base =
   System.get_env("SECRET_KEY_BASE") ||
