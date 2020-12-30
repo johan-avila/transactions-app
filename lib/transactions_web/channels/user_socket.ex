eaaf22c4841c1207d7345d474ac2f7a2d8f9ca13
Johan <johan-avila@outlook.com>
2020-12-29 23:52:29 -0600

    error: websocket doesn't close

--- lib/transactions_web/channels/user_socket.ex
+++ lib/transactions_web/channels/user_socket.ex
@@ -19,10 +19,9 @@ defmodule TransactionsWeb.UserSocket do
   def connect(_params, socket, _connect_info) do
     {:ok, socket}
   end
-  
+
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket,
-    timeout: 45_000
+  transport :websocket, Phoenix.Transports.WebSocket
 
   # Socket id's are topics that allow you to identify all sockets for a given user:
   #
