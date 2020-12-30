3151d93e7b00bd4b63eefc1dbc96726883d07c91
Johan <johan-avila@outlook.com>
2020-12-29 23:01:20 -0600

    config sockets with webpack

--- lib/transactions_web/endpoint.ex
+++ lib/transactions_web/endpoint.ex
@@ -13,11 +13,8 @@ defmodule TransactionsWeb.Endpoint do
   socket "/socket", TransactionsWeb.UserSocket,
     websocket: true,
     longpoll: false
-    
-  socket "/live", Phoenix.LiveView.Socket, websocket: [
-    connect_info:[
-      session: @session_options
-  ]]
+
+  socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info:[session: @session_options]]
 
   # Serve at "/" the static files from "priv/static" directory.
   #
