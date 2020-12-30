fd8f025b8b6f78cb0c5fec274b4137385b448067
Johan <johan-avila@outlook.com>
2020-12-30 04:12:28 -0600

    Intento casi final

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,11 +21,11 @@ let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("
 let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
 
 // Show progress bar on live navigation and form submits
-// window.addEventListener("phx:page-loading-start", info => NProgress.start())
-// window.addEventListener("phx:page-loading-stop", info => NProgress.done())
+window.addEventListener("phx:page-loading-start", info => NProgress.start())
+window.addEventListener("phx:page-loading-stop", info => NProgress.done())
 
 // connect if there are any LiveViews on the page
-liveSocket.connect()
+// liveSocket.connect()
 
 // expose liveSocket on window for web console debug logs and latency simulation:
 // >> liveSocket.enableDebug()
