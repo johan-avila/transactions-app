2db22e13e604eaf2f8ae89ec74894e4fff0bac24
Johan <johan-avila@outlook.com>
2020-12-30 00:27:33 -0600

    add test route

--- lib/transactions_web/router.ex
+++ lib/transactions_web/router.ex
@@ -18,9 +18,18 @@ defmodule TransactionsWeb.Router do
   scope "/", TransactionsWeb do
     pipe_through :browser
 
-    live "/", PageLive, :index
-    live "/users", UserLive, :index
-    live "/transactions", MovementLive, :index
+    # live "/", PageLive, :index
+    # live "/users", UserLive, :index
+    # live "/transactions", MovementLive, :index
+    ####PUEBAS###########
+    live "/pruebas", PruebaLive.Index, :index
+    live "/pruebas/new", PruebaLive.Index, :new
+    live "/pruebas/:id/edit", PruebaLive.Index, :edit
+
+    live "/pruebas/:id", PruebaLive.Show, :show
+    live "/pruebas/:id/show/edit", PruebaLive.Show, :edit
+    ####PUEBAS###########
+
   end
 
   # Other scopes may use custom stacks.
