c7cfdc1a35cfab8217a7a6951391c3b279511701
Johan <johan-avila@outlook.com>
2021-01-18 21:32:02 -0600

    mysql bug test 2

--- config/test.exs
+++ config/test.exs
@@ -5,11 +5,13 @@ use Mix.Config
 # The MIX_TEST_PARTITION environment variable can be used
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
+# database: "transactions_test#{System.get_env("MIX_TEST_PARTITION")}",
+
 config :transactions, Transactions.Repo,
-  username: "root",
-  password: "password",
-  database: "transactions_test#{System.get_env("MIX_TEST_PARTITION")}",
-  hostname: "192.168.0.3",
+  username: "uknwegb7nzrso8ec",
+  password: "Hs1RWjdwM2GfVf2IPI9v",
+  database: "bmmoxnz9sjkl6ogdsuul",
+  hostname: "bmmoxnz9sjkl6ogdsuul-mysql.services.clever-cloud.com",
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
